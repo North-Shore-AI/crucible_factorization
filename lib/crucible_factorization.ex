e967e6444c504ff54b5f1326f34c90cf9a109533
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2026-05-22 12:40:37 -1000

    Initial Mix scaffold

---
+++ lib/crucible_factorization.ex
@@ -0,0 +1,18 @@
+defmodule CrucibleFactorization do
+  @moduledoc """
+  Documentation for `CrucibleFactorization`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CrucibleFactorization.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/crucible_factorization_test.exs
@@ -0,0 +1,8 @@
+defmodule CrucibleFactorizationTest do
+  use ExUnit.Case
+  doctest CrucibleFactorization
+
+  test "greets the world" do
+    assert CrucibleFactorization.hello() == :world
+  end
+end
